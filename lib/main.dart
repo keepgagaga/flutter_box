d43b10f897285feaf9f101e68ce3f0b5699d8b94
sand <z@ruiruizhang.com>
2023-03-16 10:47:07 +0800

    modify import path name

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_box/common/themeConfig.dart';
+import 'package:flutter_box/common/ThemeConfig.dart';
 import 'package:provider/provider.dart';
 import 'package:window_manager/window_manager.dart';
 
--- lib/pages/web/Home.dart
+++ lib/pages/web/Home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_box/common/themeConfig.dart';
+import 'package:flutter_box/common/ThemeConfig.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
